53720a1458a2f6318aa0d7aa3cbaa9ac32f601f2
Shubham Tidke <tidkeshubham826@gmail.com>
2026-06-25 17:28:23 +0530

    --

--- accounts/models.py
+++ accounts/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.conf import settings
-
+######################################################################################
 
 class User(AbstractUser):
     role = models.CharField(
--- hostels/models.py
+++ hostels/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.conf import settings
+######################################################################################
 
 class Hostel(models.Model):
     hostel_name = models.CharField(max_length=100)
